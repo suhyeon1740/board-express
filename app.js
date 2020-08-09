dc6d9c2caa0508ff22ce919ad0600d2a45bb4439
elly <suhyeon1740@gmail.com>
2020-08-09 17:51:44 +0900

    메인화면에 이미지 추가

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const port = 3000
 
 app.use(express.urlencoded({ extended: false }))
 app.use(compression()) //gzip 방식으로 압축
+app.use(express.static("public"))
 
+// 미들웨어: 서버가 요청을 받아들이는 것과 응답을 전송하는 것 사이에서 작동하는 코드
+// 간단하게 말하면 클라이언트에게 요청이 오고 그 요청을 보내기 위해 응답하려는 중간(미들)에 
+// 목적에 맞게 처리를 하는, 말하자면 거쳐가는 함수들
 app.get('*', (req, res, next) => { // 모든 get방식 요청에서만 글 목록 가져옴
     fs.readdir("./data", (error, list) => {
         req.list = list
@@ -23,7 +27,7 @@ app.get("/", (req, res) => {
         const html = template.htmlTemplate(
             title,
             list,
-            `<h2>${title}</h2><div>${contents}</div>`,
+            `<h2>${title}</h2><div>${contents}</div><img src="/images/hello.jpg" width="200">`,
             `<a href="/create"> create</a>`
         )
         res.send(html)
